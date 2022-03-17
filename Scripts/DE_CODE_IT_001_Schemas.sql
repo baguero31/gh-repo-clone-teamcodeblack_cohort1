abeaae89b77ea2c3f3187f7587dd41f676f9ffb6
Alex Horton <alex.d.horton95@gmail.com>
2022-03-17 15:18:36 -0500

    Added Schema script and folder

---
+++ Scripts/DE_CODE_IT_001_Schemas.sql
@@ -0,0 +1,64 @@
+/*
+	De_Code_IT_001 - Temp Table Schemas 
+	Version 1.0.0
+*/
+
+create table TempBrenda
+(
+	[u_id] INT NOT NULL PRIMARY KEY,
+	[u_name] VARCHAR(30) NULL,
+	[u_address] VARCHAR(30) NULL,
+	[u_city] VARCHAR(30) NULL,
+	[u_state] CHAR(2) NULL,
+	[u_zipCode] VARCHAR(10) NULL
+)
+
+create table TempZak
+(
+	[u_id] INT NOT NULL PRIMARY KEY,
+	[u_name] VARCHAR(30) NULL,
+	[u_address] VARCHAR(30) NULL,
+	[u_city] VARCHAR(30) NULL,
+	[u_state] CHAR(2) NULL,
+	[u_zipCode] VARCHAR(10) NULL
+)
+
+create table TempIssac
+(
+	[u_id] INT NOT NULL PRIMARY KEY,
+	[u_name] VARCHAR(30) NULL,
+	[u_address] VARCHAR(30) NULL,
+	[u_city] VARCHAR(30) NULL,
+	[u_state] CHAR(2) NULL,
+	[u_zipCode] VARCHAR(10) NULL
+)
+
+create table TempRachelle
+(
+	[u_id] INT NOT NULL PRIMARY KEY,
+	[u_name] VARCHAR(30) NULL,
+	[u_address] VARCHAR(30) NULL,
+	[u_city] VARCHAR(30) NULL,
+	[u_state] CHAR(2) NULL,
+	[u_zipCode] VARCHAR(10) NULL
+)
+
+create table TempLaResha
+(
+	[u_id] INT NOT NULL PRIMARY KEY,
+	[u_name] VARCHAR(30) NULL,
+	[u_address] VARCHAR(30) NULL,
+	[u_city] VARCHAR(30) NULL,
+	[u_state] CHAR(2) NULL,
+	[u_zipCode] VARCHAR(10) NULL
+)
+
+create table TempCharity
+(
+	[u_id] INT NOT NULL PRIMARY KEY,
+	[u_name] VARCHAR(30) NULL,
+	[u_address] VARCHAR(30) NULL,
+	[u_city] VARCHAR(30) NULL,
+	[u_state] CHAR(2) NULL,
+	[u_zipCode] VARCHAR(10) NULL
+)
