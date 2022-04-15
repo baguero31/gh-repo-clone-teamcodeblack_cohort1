aab374b94911acf686ad3093609733ee819dd4fe
unknown <zakomaha@gmail.com>
2022-04-14 20:04:06 -0500

    add bank schema

--- Scripts/BANK/sql J&D bank project.sql
+++ Scripts/BANK/sql J&D bank project.sql
@@ -1,15 +1,23 @@
+use CheckRegister
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Vendors]') AND type in (N'U'))
+BEGIN
 create table Vendors (
-[VendorName] varchar(25) primary key not null
-,VendorID int not null
+ VendorID int primary key not null
+,[VendorName] varchar(25) not null
 ,VendorType varchar(25) not null
 )
+end
 
-Create table [Checkingtransactions] (
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GeneralLedger]') AND type in (N'U'))
+BEGIN
+Create table [GeneralLedger] (
 [date] datetime2 Primary key not null,
 [transationamount] money not null,
-[description] varchar(100)not null,
+[description] varchar(100) null,
 balance money not null,
 creditordebit bit not null,
 cleared bit	not null,
-fee bit not null,
+fee bit not null
 )
+end
