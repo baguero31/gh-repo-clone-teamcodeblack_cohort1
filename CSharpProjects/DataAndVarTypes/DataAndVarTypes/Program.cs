07e08d10e7c6d0379461e65011ff647443fcef5d
Alex Horton <alex.d.horton95@gmail.com>
2022-04-14 20:12:00 -0500

    Created the following .NET 5.0 C# Tutorial Applications: * HelloWorld * DataAndVarTypes * Conditionals

---
+++ CSharpProjects/DataAndVarTypes/DataAndVarTypes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DataAndVarTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * C# Data Types
+             * 
+             * DATA TYPES can be known by many names (variables, containers, types).  In C#, 
+             * you will use an abundance of different data types based on what you are attempting
+             * to solve for.
+             * 
+             * C# is generally broken into two different types - Value Types and Reference Types
+             * 
+             * VALUE TYPES hold the data value inside its own memory space.  Value Types usually hold a value.
+             * REFERENCE TYPES store the address of where the value is being stored.
+             * 
+             * Below are a few TYPES of Data Types that are available for you to use in C#
+             * 
+            */
+
+            //Value Types
+
+
+            //Reference Types
+
+        }
+    }
+}
