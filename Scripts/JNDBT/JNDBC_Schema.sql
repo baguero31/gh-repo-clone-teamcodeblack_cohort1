0bf0f18ab6fe5fa4e191a113996200909c6be14f
Alex Horton <alex.d.horton95@gmail.com>
2022-04-14 20:24:21 -0500

    CC-39 - Create CheckRegister project database and schemas * Changed file for schema from BANK to JNDBT * Updated Schema File Name * Added Credits and Debits table to schema.

---
+++ Scripts/JNDBT/JNDBC_Schema.sql
@@ -0,0 +1,68 @@
+use CheckRegister
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Vendors]') AND type in (N'U'))
+BEGIN
+	create table Vendors (
+	 VendorID int primary key not null
+	,[VendorName] varchar(25) not null
+	,VendorType varchar(25) not null
+	)
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GeneralLedger]') AND type in (N'U'))
+BEGIN
+	Create table [GeneralLedger] (
+		[date] datetime2 Primary key not null,
+		[transationamount] money not null,
+		[description] varchar(100) null,
+		balance money not null,
+		creditordebit bit not null,
+		cleared bit	not null,
+		fee bit not null
+	)
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Debits]') AND type in (N'U'))	
+BEGIN
+	CREATE TABLE [dbo].[Debits](
+		[DebitID] UNIQUEIDENTIFIER DEFAULT NEWID() NOT NULL,
+		[TransactionDate] [datetime] NOT NULL,
+		[TransactionAmount] [money] NOT NULL,
+		[VendorID] [VARCHAR](38) NOT NULL,
+		[MemoLine] [varchar](200) NULL,
+		[DebitCleared] [bit] NOT NULL,
+	 CONSTRAINT [PK_Debit] PRIMARY KEY CLUSTERED 
+	(
+		[DebitID] ASC,
+		[TransactionDate] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) ON [PRIMARY]
+	END
+ELSE
+BEGIN
+	PRINT 'Debits Table already exists'
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Credits]') AND type in (N'U'))	
+BEGIN
+	CREATE TABLE [dbo].[Credits](
+		[CreditID] UNIQUEIDENTIFIER DEFAULT NEWID() NOT NULL,
+		[TransactionDate] [datetime] NOT NULL,
+		[TransactionAmount] [money] NOT NULL,
+		[VendorID] [VARCHAR](38) NOT NULL,
+		[MemoLine] [varchar](200) NULL,
+		[CreditCleared] [bit] NOT NULL,
+	 CONSTRAINT [PK_Credit] PRIMARY KEY CLUSTERED 
+	(
+		[CreditID] ASC,
+		[TransactionDate] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) ON [PRIMARY]
+END
+ELSE
+BEGIN
+	PRINT 'Credits Table already exists'
+END
+GO
