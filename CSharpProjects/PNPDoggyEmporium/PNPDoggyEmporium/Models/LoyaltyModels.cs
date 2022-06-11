c6177a9acb53f072a826a0890a5cb24707e72477
Alex Horton <alex.d.horton95@gmail.com>
2022-06-11 00:10:35 -0500

    Added Doggy Emporium from a previous live stream

---
+++ CSharpProjects/PNPDoggyEmporium/PNPDoggyEmporium/Models/LoyaltyModels.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace PNPDoggyEmporium.Models
+{
+    #region Base Classes
+    /// <summary>
+    /// Base Classes
+    /// </summary>
+    public class Customers
+    {
+        public DateTime TransactionDate { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal TransactionPoints { get; set; }
+    }
+
+    public class LoyaltyRanges
+    {
+        public long LoyaltyID { get; set; }
+        public int LowRange { get; set; }
+        public int HiRange { get; set; }
+    }
+    #endregion
+
+    #region Query Models
+
+    /// <summary>
+    /// Total points per customer with the ranges
+    /// </summary>
+    public class TPPCustomers
+    {
+        public string LoyaltyType { get; set; }
+        public int TotalPoints { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+
+    public class APPCustomers
+    {
+        public int AvgPoints { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+
+    public class DupCustomers 
+    {
+        public int CountNum { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+
+    #endregion
+}
